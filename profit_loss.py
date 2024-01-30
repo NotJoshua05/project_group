efcf3af611f50944479f7229f63232907af60ed8
Xing Rong <s10258375@connect.np.edu.sg>
2024-01-30 19:57:06 +0800

    made different list

--- profit_loss.py
+++ profit_loss.py
@@ -1,5 +1,14 @@
 from common import readcsv,findTrend,top3List,getDailyDiff,getNegativeDaily
 
 def ComputeProfitAndLos(file):
-    grossprofit = []
-    grossprofit = readcsv("Profits_and_Loss.csv")
+    netprofit = []
+    netprofit = readcsv("Profits_and_Loss.csv")
+    #dailyDiffList = getDailyCOHDiff(CashOnHand)
+    getDailyDiff = getDailyDiff(0,4,netprofit)
+    DailyNegList = getNegativeDaily(dailyDiffList)
+    trend = findTrend[dailyDiffList]
+    #print(trend)
+    top3Highest=()
+    top3Highest= top5list(dailyDiffList,trend)
+    #print(top3Highest)
+    
