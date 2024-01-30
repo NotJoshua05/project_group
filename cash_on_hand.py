c84d8e234944a93d36f309538ff60a00444717d3
Jermaine <S10255984C@connect.np.edu.sg>
2024-01-30 14:41:11 +0800

    2:41 Changes 2

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -12,3 +12,8 @@ def ComputCOH(file):
     #print(top3Highest)
     #for item in top3Highest:
         #print("day:",item[1],"diff:",item[0])
+    if(trend == "Increasing"):
+        print('[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
+        print(f'[HIGHEST CASH SURPLUS] DAY: {top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}')
+        file.write('[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
+        file.write(f'[HIGHEST CASH SURPLUS] DAY: {top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}\n')
