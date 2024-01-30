98aa32088a06a2fd6bc8847c9ac737619300073f
Xing Rong <s10258375@connect.np.edu.sg>
2024-01-30 13:42:11 +0800

    Create a path object

--- common.py
+++ common.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 # Import csv module
 import csv
+def readcsv(filename):
+    csvdata=[]
+#create a path object for text.txt
+    file_path = Path.cwd()/"csv.reports"/filename    
