adbca83cafbf29e7a5d5cfa67ed432193dbdd317
Xing Rong <s10258375@connect.np.edu.sg>
2024-01-31 22:29:21 +0800

    Making final changes

--- profit_loss.py
+++ profit_loss.py
@@ -17,9 +17,10 @@ def ComputeProfitAndLos(file):
         file.write("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
         file.write(f"[HIGHEST NET PROFIT SURPLUS] DAY:{top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}\n")
     elif(trend == "Decreasing"):
-        print("[NET PRFIT DEFICIT] NET PROFIT ON EACH DAY IS LOWER THAN THE PREVIOUS DAY")
+        print("[NET PROFIT DEFICIT] NET PROFIT ON EACH DAY IS LOWER THAN THE PREVIOUS DAY")
         print(f"[HIGHEST PRFOIT DEFICIT] DAY: {top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}")
         file.write("[NET PROFIT DEFICIT] NET PROFIT ON EACH DAY IS LOWER THAN THE PREVIOUS DAY\n")
+        file.write("[HIGHEST PROFIT DEFICIT] DAY: {top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}\n")
     elif(trend == "Fluctuating"):
         for item in dailyNegList:
             print(f"[NET PROFIT DEFICIT] DAY: {item[1]} AMOUNT: SGD{item[0]}")
@@ -27,6 +28,6 @@ def ComputeProfitAndLos(file):
         print (f"[HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}")
         print (f"[2ND HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[1][1]} AMOUNT: SGD{top3Highest[1][0]}")
         print (f"[3RD HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[2][1]} AMOUNT: SGD{top3Highest[2][0]}")
-        print (f"[HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}\n")
-        file.write (f"[2ND HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[0][1]} AMOUNT: SGD{top3Highest[1][0]}\n")
-        file.write (f"[3RD HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[0][1]} AMOUNT: SGD{top3Highest[2][0]}\n")
+        file.write (f"[HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[0][1]} AMOUNT: SGD{top3Highest[0][0]}\n")
+        file.write (f"[2ND HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[1][1]} AMOUNT: SGD{top3Highest[1][0]}\n")
+        file.write (f"[3RD HIGHEST NET PROFIT DEFICIT] DAY:{top3Highest[2][1]} AMOUNT: SGD{top3Highest[2][0]}\n")
